ce748bce943df357f7dd0f2c99430b8ae2fafc8e
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2023-11-04 12:59:07 +0800

    add simple calculator

--- js/app.js
+++ js/app.js
@@ -1,7 +1,51 @@
-// alert("hello world!")
 
-const btn = document.querySelector("button")
+// var a = 100
 
-btn.addEventListener("click", function(){
-    alert("hello world!")
-})
+// html element
+const aInput = document.querySelector("input#a")
+
+const bInput = document.querySelector("input#b")
+
+const operatorSelect = document.querySelector("select#operator")
+
+const resButton = document.querySelector("button")
+
+const resultSpan = document.querySelector("span#result")
+
+// html element's attributes
+console.log(aInput.style)
+console.log(aInput.className)
+
+// html element's property
+console.log(aInput.value)
+
+function calculate() {
+    const aVal = parseFloat(aInput.value)
+    const bVal = parseFloat(bInput.value)
+
+    // const result = aVal + bVal
+    let result
+
+    const op = operatorSelect.value
+    if (op === "+") {
+        result = aVal + bVal
+    }
+
+    if (op === "-") {
+        result = aVal - bVal
+    }
+
+    if (op === "*") {
+        result = aVal * bVal
+    }
+
+    if (op === "/") {
+        result = aVal / bVal
+    }
+
+    resultSpan.innerText = result
+    // alert(result)
+}
+
+// property
+resButton.addEventListener("click", calculate)
