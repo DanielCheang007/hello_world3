a4491cc007b8b7b98d4df67595fd40018dcc967c
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2023-11-18 11:52:34 +0800

    fix dropfood function

--- 04_snake/js/app.js
+++ 04_snake/js/app.js
@@ -11,7 +11,9 @@ const TOTAL_CELLS = COLS * ROWS
 const cellWidth = WIDTH / COLS
 const cellHeight = HIGHT / ROWS
 
-const cells = []
+const CELLS = []
+
+
 for (let i = 0; i < TOTAL_CELLS; i++) {
     const cell = document.createElement("div")
     cell.classList.add("cell")
@@ -19,13 +21,39 @@ for (let i = 0; i < TOTAL_CELLS; i++) {
     cell.style.height = cellHeight + "px"
     pg.appendChild(cell)
 
-    cells.push(cell)
+    CELLS.push(cell)
 }
 
 const snake = [31, 32, 33, 43]
 
 for (let i = 0; i < snake.length; i++) {
     const idx = snake[i]
-    const cell = cells[idx]
+    const cell = CELLS[idx]
     cell.classList.add("has-snake")
 }
+
+const pgArray = []
+for (let i = 0; i < TOTAL_CELLS; i++) {
+    pgArray.push(i)
+}
+
+function dropFood() {
+    // filter cells that not have snake body
+    const availableCells = pgArray.filter(function (el) {
+        return !snake.includes(el)
+    })
+
+    // get a random idx from available cells
+    let randomPos = Math.floor(Math.random() * availableCells.length)
+
+    // get the cell index
+    const cellIdx = availableCells[randomPos]
+
+    // get the cell
+    const cell = CELLS[cellIdx]
+
+    // set the food class
+    cell.classList.add("has-food")
+}
+
+dropFood()
