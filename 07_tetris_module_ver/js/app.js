55bb47ad73f498ce772893ea9ad771c698990990
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2023-12-16 11:23:22 +0800

    add tetris module ver

---
+++ 07_tetris_module_ver/js/app.js
@@ -0,0 +1,4 @@
+
+import { render } from "./render_html.js"
+
+render()
---
+++ 07_tetris_module_ver/js/render_html.js
@@ -0,0 +1,15 @@
+const COLS = 10
+const ROWS = 20
+const TOTAL = COLS * ROWS
+
+function render() {
+  // TODO: ... 
+  alert("hi! I am render!")
+}
+
+// export {
+//   COLS,
+//   ROWS,
+//   TOTAL,
+//   render
+// }
